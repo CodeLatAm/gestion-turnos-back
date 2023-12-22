0e1f8c6330726fe08573b0b96e0de4f7a2718324
Abel3581 <marzoa3581@gmail.com>
2023-12-21 23:44:04 -0300

    act

---
+++ src/main/java/com/getion/turnos/exception/ProfileAlreadyExist.java
@@ -0,0 +1,8 @@
+package com.getion.turnos.exception;
+
+public class ProfileAlreadyExist extends RuntimeException{
+
+    public ProfileAlreadyExist(String message){
+        super(message);
+    }
+}
